e93db9d1ec145dafcf7564be392c2a7e666a7cf1
Barcan Silviu-Ioan <silviu_barcan@yahoo.com>
2025-12-21 13:25:45 +0200

    fix: comment vote page refreshes

--- DockerProject/wwwroot/js/site.js
+++ DockerProject/wwwroot/js/site.js
@@ -26,3 +26,46 @@ function toggleChildren(commentId) {
     }
 }
 
+function voteComment(commentId, isUpVote) {
+    let formData = new FormData();
+    
+    formData.append('commentId', commentId);
+    formData.append('isUpVote', isUpVote);
+
+    fetch('/Comments/Vote/', {
+        method: 'POST',
+        body: formData,
+    }).then(response => {
+        if (response.ok)
+            return response.json();
+        throw Error(response.statusText);
+    }).then(data => {
+        if (data.success) {
+            const scoreElement = document.getElementById('score-' + commentId);
+            if (scoreElement)
+                scoreElement.innerText = data.score;
+            const btnUp = document.getElementById('btn-up-' + commentId);
+            const iconUp = document.getElementById('icon-up-' + commentId);
+
+            if (data.userStatus === 1) { // upvote
+                btnUp.classList.replace('text-muted', 'text-warning');
+                iconUp.classList.replace('bi-caret-up', 'bi-caret-up-fill');
+            } else { // not-upvoted
+                btnUp.classList.replace('text-warning', 'text-muted');
+                iconUp.classList.replace('bi-caret-up-fill', 'bi-caret-up');
+            }
+
+            const btnDown = document.getElementById('btn-down-' + commentId);
+            const iconDown = document.getElementById('icon-down-' + commentId);
+
+            if (data.userStatus === -1) { // downvote
+                btnDown.classList.replace('text-muted', 'text-warning');
+                iconDown.classList.replace('bi-caret-down', 'bi-caret-down-fill');
+            } else { // not-downvoted
+                btnDown.classList.replace('text-warning', 'text-muted');
+                iconDown.classList.replace('bi-caret-down-fill', 'bi-caret-down');
+            }
+        }
+    }).catch(error => console.error('Error:', error));
+}
+
